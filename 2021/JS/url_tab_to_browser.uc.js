913b6f8d45652441547891f01aab3104ab2c937b
tortious <unjustlyenriched@protonmail.com>
2021-01-12 16:36:39 -0500

    Create url_tab_to_browser.uc.js

---
+++ 2021/JS/url_tab_to_browser.uc.js
@@ -0,0 +1,45 @@
+
+(function() {
+	let {Services} = Components.utils.import("resource://gre/modules/Services.jsm", {});
+	
+	function processLoadedWindow(win) {
+		// This function will be called every time a new window opens
+		// This might include non-browser windows as well
+		
+		let urlbar = win.document.getElementById("urlbar");
+		if(urlbar !== null) {
+			urlbar.addEventListener("keydown", function(e) {
+				if(e.keyCode == 9) {
+					e.target.ownerDocument.defaultView.gBrowser.selectedTab.linkedBrowser.focus();
+					e.preventDefault();
+					e.stopPropagation();
+				}
+			});
+		}
+	}
+	
+	function processWindow(win) {
+		// I'm not sure if this verification is needed or not
+		let readyState = win.document.readyState;
+		if(readyState === "complete" || readyState === "interactive") {
+			processLoadedWindow(win);
+			
+		} else {
+			win.addEventListener("load", function(){processLoadedWindow(win)});
+		}
+	}
+	
+	// Make sure to process the already existing window
+	processWindow(window);
+	
+	function WindowObserver() {
+		this.observe = function(subject, topic, data) {
+			if(topic === "domwindowopened") {
+				processWindow(subject);
+			}
+		}
+	}
+	
+	Services.ww.registerNotification(new WindowObserver());
+	
+})();
